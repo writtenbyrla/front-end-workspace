3b0794d91c160117408a0d2a726878fc415c6b76
writtenbyrla <gnnvistavie@gmail.com>
2023-07-25 17:42:33 +0900

    feat: js(정규표현식)

--- 3_JavaScript/js/실습문제2.js
+++ 3_JavaScript/js/실습문제2.js
@@ -1,14 +1,87 @@
+// window.addEventListener('scroll', function(){
+//     console.log(window.pageYOffset); // deprecated(사용X)
+//     console.log(window.scrollY);
+//     console.log(document.body.offsetHeight); // 스크롤 끝까지 내렸을때 높이
+//     console.log(window.innerHeight);
+//     // 스크롤 내렸을때 높이 차이
+// });
+
+let maxScrollValue;
+const progressBar = document.querySelector('.progress-bar');
+
+function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    // 전체 스크롤 할 수 있는 범위 
+    //     = 바디 전체 높이 - 윈도우 현재 창의 높이
+}
+
 window.addEventListener('scroll', function(){
-    console.log(window.pageYOffset); // deprecated(사용X)
-    console.log(window.scrollY);
-    console.log(document.body.offsetHeight); // 스크롤 끝까지 내렸을때 높이
-    console.log(window.innerHeight);
-    // 스크롤 내렸을때 높이 차이
+    progressBar.style.width = (window.scrollY / maxScrollValue) * 100 + '%';
 });
 
-// const bar = document.querySelector('.progress-bar');
-// function scrollbar () {
-//     bar.
-// }
+window.addEventListener('resize', resizeHandler);
+resizeHandler();
 
-// bar.addEventListener('scroll', scrollbar);
+// navigation click event
+const nav = document.querySelector('nav');
+let current;
+
+function navHandler(e){
+
+    if(current){
+        current.style.backgroundColor = 'transparent';
+        current.style.color='black';
+    }
+    
+    if(e.target!==e.currentTarget) {
+        e.target.style.backgroundColor = 'black';
+        e.target.style.color = 'white';
+        current = e.target;
+    }
+
+    // // 선택한 메뉴의 색상을 바꿈
+    // e.target.style.backgroundColor = 'black';
+    // e.target.style.color = 'white';
+    
+    // // 지금 선택한 메뉴를 current에 담음
+    // current = e.target;
+}
+
+nav.addEventListener('click', navHandler);
+
+// mouse wheel event
+const section1 = document.querySelector('#section1');
+const section2 = document.querySelector('#section2');
+const section3 = document.querySelector('#section3');
+
+let currentSection = section1;
+
+window.addEventListener('wheel', function(event){
+    if(event.deltaY > 0){ // 휠을 아래로 내린 경우
+        if(currentSection === section1){
+            this.window.scrollTo({top: section2.offsetTop, behavior: 'smooth'});
+            currentSection = section2;
+        } else if(currentSection === section2){
+            this.window.scrollTo({top: section3.offsetTop, behavior: 'smooth'});
+            currentSection = section3;
+        }
+    } else { // 휠을 위로 올린 경우
+        if(currentSection === section3){
+            this.window.scrollTo({top: section2.offsetTop, behavior: 'smooth'});
+            currentSection = section2;
+        } else if(currentSection === section2){
+            this.window.scrollTo({top: section1.offsetTop, behavior: 'smooth'});
+            currentSection = section1;
+        }
+    }
+});
+
+// scroll - animation 살짝
+window.addEventListener('scroll', function(){
+    if(section2.getBoundingClientRect().top===0){
+        section2.children[0].classList.add('text-animation');
+    } else {
+        section2.children[0].classList.remove('text-animation');
+    }
+
+});
--- 3_JavaScript/js/연습문제3.js
+++ 3_JavaScript/js/연습문제3.js
@@ -5,15 +5,24 @@ const result = document.querySelector('.main-result');
 const span = document.querySelector('.click span');
 let count = 0;
 
+// click 눌렀을때 실행되는 함수
 function change(event){
+
+    // 횟수 카운트
     span.innerHTML = ++count;
 
+    // 1~3까지 랜덤값 배열로 담기
     const srcArr = [Math.floor(Math.random()*3)+1, Math.floor(Math.random()*3)+1, Math.floor(Math.random()*3)+1];
 
+
    for(let i = 0; i<imgList.length; i++){
+        // 3가지 그림 랜덤으로 src 속성 바꾸기
         imgList[i].setAttribute('src', `../resources/spy${srcArr[i]}.jpg`);
+
+        // 그림 3개 모두 일치할 경우
         if(srcArr[0]===srcArr[1]&&srcArr[1]==srcArr[2]) {
             result.innerHTML = "Congratulation!! Press restart to play again!!";
+            
             // 클릭 비활성화
             click.setAttribute("disabled", "disabled");
             
@@ -22,6 +31,7 @@ function change(event){
    
 }
 
+// restart 눌렀을때 실행되는 함수
 function reset(event){
    for(let i = 0; i<imgList.length; i++){
         imgList[i].setAttribute('src', `../resources/spy1.jpg`);
@@ -29,6 +39,7 @@ function reset(event){
     count = 0;
     result.innerHTML="";
     span.innerHTML = "";
+    // 속성 삭제
     click.removeAttribute("disabled");
 }
 
