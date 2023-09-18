7c19cc243a271edea4c2c075df47a2cf62c58e0b
writtenbyrla <gnnvistavie@gmail.com>
2023-09-18 17:47:40 +0900

    jsx

--- 7-jsx2/src/App.js
+++ 7-jsx2/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const setCount = () => {
+    setCounter(counter + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Total clicks: {counter}</h1>
+      <button onClick={setCount}>Click!</button>
     </div>
   );
-}
+};
 
 export default App;
